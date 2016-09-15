f0ab59a0bdb1773edbd097b200aaaf937dfcbda0
ChaniceStl <chanicestlouis2020@gmail.com>
2016-09-15 14:26:56 -0400

    set up react project

---
+++ public/scripts/app.js
@@ -0,0 +1,42 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+//require react and react dom 
+//render to reactdom
+
+import Form from "./form.js"
+//create a variable that will hold our main class
+var App = React.createClass({
+    render() {
+      return ( 
+      	< div >
+      		<h1>Doo-Doo</h1>
+        < /div>
+      )
+    }
+  })
+  
+  //use the google API and its key. to get the lat and lng from the address we entered in the input box 
+  // in our success function we call out our second API and store the lat n lng in variable 
+  //use the congress API to get the nearest congress person within that lat lng 
+
+
+
+//concat the user input 
+//Store input in a variable.
+
+//make ajax request with a url and a success function to do something with the API.
+//
+
+
+
+//input box to get the user value.
+//button to submit the user value
+
+ReactDOM.render(
+<App />, 
+ document.getElementById("root")
+)
+
+
+
